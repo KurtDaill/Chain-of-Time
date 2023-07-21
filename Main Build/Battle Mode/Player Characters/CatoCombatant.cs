65835bf3442ac59ddc1b4b04e3e2cd59473847ca
KurtDaill <31542404+KurtDaill@users.noreply.github.com>
2023-07-20 19:25:15 -0500

    Migrated to Laptop
    For Cooworking

--- Main Build/Battle Mode/Player Characters/CatoCombatant.cs
+++ Main Build/Battle Mode/Player Characters/CatoCombatant.cs
@@ -2,9 +2,9 @@ using System;
 using Godot;
 
 public partial class CatoCombatant : PlayerCombatant{
-    public override void _Ready()
+	public override void _Ready()
 	{
 		base._Ready();
-        name = "Cato";
+		name = "Cato";
 	}
 }
