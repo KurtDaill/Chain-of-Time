10e7fe1766aa55e2221e158f630c3c6c2aff0abf
KurtDaill <31542404+KurtDaill@users.noreply.github.com>
2023-07-27 17:14:13 -0500

    Cutscene Plugin Created

--- Main Build/Cutscenes/CutsceneDirector.cs
+++ Main Build/Cutscenes/CutsceneDirector.cs
@@ -4,6 +4,17 @@ using System.Collections.Generic;
 public partial class CutsceneDirector : Node3D
 {
     public bool enabled = true;
+    public Dictionary<string, Actor> cast;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        foreach(Node node in this.GetChildren()){
+            switch(node.GetScript()){
+
+            }
+        }
+    }
     public void StartCutscene(){
 
     }
