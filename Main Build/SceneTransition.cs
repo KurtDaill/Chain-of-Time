e3d712ea4db674f4d7db4fed9765ecee38a99960
KurtDaill <31542404+KurtDaill@users.noreply.github.com>
2023-08-23 19:17:09 -0500

    City Built, Level Transitions Humming

---
+++ Main Build/SceneTransition.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+public partial class SceneTransition : GameplayMode {
+    [Export(PropertyHint.File)]
+    string targetScenePath;
+
+    
+    /*This function is used for the current mode to set everything up how it would like it before having to accepting Player Input.
+    It's configured as a task such that visual effects lasting longer than a frame can be handled under this function.*/
+    public override Task StartUp(){
+        GetTree().ChangeSceneToFile(targetScenePath);
+        return null;
+    }
+}
