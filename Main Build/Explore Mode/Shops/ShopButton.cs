19f6cd167e473812043955d227bb466e174e1627
KurtDaill <31542404+KurtDaill@users.noreply.github.com>
2023-10-31 11:42:33 -0500

    Progress on Shop Interface

--- Main Build/Explore Mode/Shops/ShopButton.cs
+++ Main Build/Explore Mode/Shops/ShopButton.cs
@@ -1,6 +1,20 @@
 using System;
 using Godot;
 
-public abstract partial class ShopButton: Sprite3D{
+public partial class ShopButton: Sprite3D{
+    private Sprite3D highlightSprite;
+    private Label3D text;
 
+    public override void _Ready()
+    {
+        base._Ready();
+        highlightSprite = this.GetNode<Sprite3D>("Highlight");
+        if(highlightSprite == null) throw new ArgumentException("Highlight not found for ShopButton: " + this.Name);
+        text = this.GetNode<Label3D>("Text");
+        if(text == null) throw new ArgumentNullException("Text not found for ShopButton: " + this.Name);
+    }
+
+    public void SetSelect(bool selected){
+        this.highlightSprite.Visible = selected;
+    }
 }
--- Main Build/Explore Mode/Shops/ShopInterface.cs
+++ Main Build/Explore Mode/Shops/ShopInterface.cs
@@ -1,34 +1,56 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Godot;
 using static GameplayUtilities;
 public partial class ShopInterface : GameplayMode{
-    public Node3D[][] shopButton;
+    public ShopButton[][] shopButton;
     private int selectButtonX, selectButtonY;
+    [Export]
+    private int defaultButtonX = 0;
+    [Export]
+    private int defaultButtonY = 0;
     public Label3D descriptionBoxTitleLine, descriptionBoxBody;
+    [Export]
+    private Camera3D shopCam;
 
     public override void _Ready()
     {
         base._Ready();
         selectButtonX = 0;
         selectButtonY = 0;
+        Node3D buttonMap = this.GetNode<Node3D>("ButtonMap");
+        if(buttonMap == null) throw new ArgumentException("Button Map not found for Shop Interface: " + Name);
+        Godot.Collections.Array<Node> buttonColumns = buttonMap.GetChildren();
+        shopButton = new ShopButton[buttonColumns.Count][];
+        for(int i = 0; i < buttonColumns.Count; i++){
+            Godot.Collections.Array<Node> currentColumn = buttonColumns[i].GetChildren();
+            shopButton[i] = new ShopButton[currentColumn.Count];
+            for(int j = 0; j < currentColumn.Count; j++){
+                shopButton[i][j] = (ShopButton) currentColumn[j];
+            }
+        }
     }
 
     public override Task StartUp(GameplayMode oldMode){
-        selectButtonX = 0;
-        selectButtonY = 0;
+        selectButtonX = defaultButtonX;
+        selectButtonY = defaultButtonY;
+        shopCam.Current = true;
         return Task.CompletedTask;
     }
 
-    public override Task<GameplayMode> RemoteProcess(double delta)
+    public override async Task<GameplayMode> RemoteProcess(double delta)
     {
         //TODO - Have the system respond to different selectButton values!
+        
         return null;
     }
 
     public override void HandleInput(PlayerInput input)
     {
         base.HandleInput(input);
+        //Turn off the old button
+        shopButton[selectButtonX][selectButtonY].SetSelect(false);
         switch(input){
             case PlayerInput.Down:
                 if(selectButtonY < shopButton[0].Length) selectButtonY++; break;
@@ -39,5 +61,7 @@ public partial class ShopInterface : GameplayMode{
             case PlayerInput.Left:
                 if(selectButtonY > 0) selectButtonX--; break;
         }
+        //Whatever the current button is gets to be visible
+        shopButton[selectButtonX][selectButtonY].SetSelect(true);
     }
 }
